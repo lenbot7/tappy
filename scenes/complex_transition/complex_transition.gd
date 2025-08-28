a1dc60a0a0560bf043e61cc2837700192e359ba2
Lenny Kay <77846195+lenbot7@users.noreply.github.com>
2025-08-28 12:23:26 +0100

    .

--- scenes/complex_transition/complex_transition.gd
+++ scenes/complex_transition/complex_transition.gd
@@ -5,5 +5,5 @@ func switch_scene() -> void:
 	get_tree().change_scene_to_packed(GameManager.next_scene)
 
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	queue_free()
